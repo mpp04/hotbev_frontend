4d998bc591f3dfb709562a8176cefe8b61249871
mpp <mpp@sdf.org>
2021-02-03 13:02:43 -0500

    troubleshooting frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  CoffeesEndpoint: 'http://ec2-52-91-28-213.compute-1.amazonaws.com:8091/boot/Coffee',
-  CoffeeDrinksEndpoint: 'http://ec2-52-91-28-213.compute-1.amazonaws.com:8091/boot/CoffeeDrinks',
-  TeasEndpoint: 'http://ec2-52-91-28-213.compute-1.amazonaws.com:8093/boot',
-  SoupsEndpoint: 'http://ec2-52-91-28-213.compute-1.amazonaws.com:8092',
+  CoffeesEndpoint: 'http://ec2-3-85-233-109.compute-1.amazonaws.com:8091/boot/Coffee',
+  CoffeeDrinksEndpoint: 'http://ec2-3-85-233-109.compute-1.amazonaws.com:8091/boot/CoffeeDrinks',
+  TeasEndpoint: 'http://ec2-3-85-233-109.compute-1.amazonaws.com:8093/boot',
+  SoupsEndpoint: 'http://ec2-3-85-233-109.compute-1.amazonaws.com:8092',
   admin: true,
 };
